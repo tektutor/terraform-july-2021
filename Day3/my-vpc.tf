056b0eab86ac6b90dd0b6969f3c877a3f7ec9f92
Jeganathan Swaminathan <mail2jegan@gmail.com>
2021-07-07 06:03:19 -0700

    Removed duplicate files.

--- Day3/my-vpc.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_vpc" "jegan_vpc" {
-	cidr_block = "192.168.0.0/16"
-
-	tags = {
-		Name = "jegan_vpc"
-	}
-}
--- Day3/subnet.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_subnet" "jegan_subnet_1" {
-	vpc_id = aws_vpc.jegan_vpc.id 
-	cidr_block = "192.168.1.0/24"
-
-	tags = {
-		Name = "jegan_subnet_1"
-	}
-}
